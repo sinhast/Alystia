67c8c2752ee1134d3956bcf2cc84edf947b8249c
Sinh <dylanv.dev@gmail.com>
2020-03-17 19:43:44 +0100

    Add a command to have server info

--- main.js
+++ main.js
@@ -28,6 +28,16 @@ Alystia.on("message", msg => {
       .delete({ timeout: 0.5 })
       .then(console.log("A message has been deleted."));
   }
+
+  if (cmd === "sinfo") {
+    const embed = new Discord.MessageEmbed()
+      .setDescription(msg.guild.name)
+      .setThumbnail(msg.guild.iconURL())
+      .addField("Membres", msg.guild.memberCount)
+      .setFooter(msg.guild.owner.user.tag, msg.guild.owner.user.avatarURL())
+      .setTimestamp();
+    msg.channel.send(embed);
+  }
 });
 
 Alystia.on("guildMemberAdd", member => {
