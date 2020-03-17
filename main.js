0c588e1742c482eef5dc4818193bff5763fd4db3
Sinh <dylanv.dev@gmail.com>
2020-03-17 11:28:04 +0100

    Add a require for settings.js and set the token setting

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+"use strict";
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const Discord = require('discord.js');
+const Settings = require('./settings.js');
+const Alystia = new Discord.Client();
+
+Alystia.login(Settings.token);
+
+Alystia.on('ready', () => {
+  console.log(`Logged in as ${Alystia.user.tag}!`);
 });
 
-client.on('message', msg => {
+Alystia.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
-
-client.login('token');
