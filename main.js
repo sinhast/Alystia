77ce8295b0ef638cc4cd4f376073993895cba40c
Sinh <dylanv.dev@gmail.com>
2020-03-17 14:55:13 +0100

    Install es-lint and es-lint config

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-"use strict";
-
-const Discord = require('discord.js');
-const Settings = require('./settings.js');
+const Discord = require("discord.js");
+const Settings = require("./settings.js");
 const Alystia = new Discord.Client();
 
 Alystia.login(Settings.token);
 
-Alystia.on('ready', () => {
+Alystia.on("ready", () => {
   console.log(`Logged in as ${Alystia.user.tag}!`);
 });
 
-Alystia.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+Alystia.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
   }
 });
