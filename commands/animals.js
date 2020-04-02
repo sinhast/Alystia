a3b9980352ef995a2bf3da6d93cdcb0e9eea3595
Sinh <dylanv.dev@gmail.com>
2020-04-02 17:52:01 +0200

    Add a command to pop an image of an specific animal with API

---
+++ commands/animals.js
@@ -0,0 +1,40 @@
+const fetch = require("node-fetch");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = async (client, message, args) => {
+  const embed = new MessageEmbed();
+  if (args[0] === "cat") {
+    const cat = await fetch("http://aws.random.cat/meow")
+      .then(res => res.json())
+      .then(json => json.file);
+
+    const embed = new MessageEmbed()
+      .setImage(cat)
+      .setFooter("Powered by 'http://aws.random.cat/meow'");
+    message.channel.send(embed);
+  } else if (args[0] === "dog") {
+    const dog = await fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(json => json.message);
+
+    const embed = new MessageEmbed()
+      .setImage(dog)
+      .setFooter("Powered by 'https://dog.ceo/api/breeds/image/random'");
+    message.channel.send(embed);
+  } else if (args[0] === "fox") {
+    const fox = await fetch("https://randomfox.ca/floof/")
+      .then(res => res.json())
+      .then(json => json.image);
+
+    const embed = new MessageEmbed()
+      .setImage(fox)
+      .setFooter("Powered by 'https://randomfox.ca/floof/'");
+    message.channel.send(embed);
+  } else {
+    const embed = new MessageEmbed()
+      .setColor("RED")
+      .setTitle(":x: Missing args")
+      .setDescription("Available Args : cat, dog & fox.");
+    message.channel.send(embed);
+  }
+};
